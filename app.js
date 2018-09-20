551407e5ab2821afc6c350fcb2edb1904d17f6f0
Eben Mallory <ebenryanmallory@gmail.com>
2018-09-20 14:42:10 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var hbs = require('hbs');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var grapesRouter = require('./routes/grapesjs');
+var portalRouter = require('./routes/portal');
+var standardtemplateRouter = require('./routes/standard-template');
+var blanktemplatelRouter = require('./routes/blank-template');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.use('/users', usersRouter);
+app.use('/grapes', grapesRouter);
+app.use('/portal', portalRouter);
+app.use('/standard-template', standardtemplateRouter);
+app.use('/blank-template', blanktemplatelRouter);
+
+module.exports = app;
