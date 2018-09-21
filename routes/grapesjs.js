2a8b930d2a2fffca3e9d0081e8cc3cc43f22d58e
Eben Mallory <ebenryanmallory@gmail.com>
2018-09-20 21:19:32 -0600

    .env

--- routes/grapesjs.js
+++ routes/grapesjs.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
+
+const firebaseData = process.env.TEST;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render(process.cwd() + '/public/grapesjs.hbs');
+  res.render(process.cwd() + '/public/grapesjs.hbs', { script: '<script>console.log("' + firebaseData + '");</script>', message: 'Hello there!' });
 });
 
 module.exports = router;
