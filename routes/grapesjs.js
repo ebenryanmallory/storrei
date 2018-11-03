0d0b7b072055de6780e33ba855e9192a237ea18d
Eben Mallory <ebenryanmallory@gmail.com>
2018-11-02 20:30:14 -0600

    added plugins

--- routes/grapesjs.js
+++ routes/grapesjs.js
@@ -4,6 +4,10 @@ require('dotenv').config();
 
 const firebaseApiKey = process.env.FIREBASE_API_KEY;
 
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
 router.get('/', function(req, res, next) {
   res.render(process.cwd() + '/public/grapesjs.hbs', { firebaseImport: firebaseApiKey }); 
 });
