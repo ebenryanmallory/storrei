0703d20e6311de75103c29a532a9f0d7a22354d1
Eben Mallory <ebenryanmallory@gmail.com>
2018-11-02 21:20:10 -0600

    added sign in

--- routes/portal.js
+++ routes/portal.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const firebaseApiKey = process.env.FIREBASE_API_KEY;
 router.get('/', function(req, res, next) {
-  res.render(process.cwd() + '/public/portal.hbs');
+  res.render(process.cwd() + '/public/portal.hbs', { firebaseImport: firebaseApiKey });
 });
 
 module.exports = router;
